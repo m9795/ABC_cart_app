3272f9d0420ae3d9fceb38d64b96333b573112f8
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-07 02:53:02 +0900

    [Add]商品一覧　情報表示できない

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,21 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @item = Item.new
+    @items = Item.all
   end
   
   def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_items_path
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   }
   root to: 'homes#top'
   get 'about' => 'homes#about'
-  resources :items, only: [:index]
+  
   namespace :public do
     resources :items, only: [:index]
     resources :customers, only: [:index, :show]
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :items, only: [:index, :new, :create]
   end
+  resources :items, only: [:index]
   # get 'public/items/index'
   # get 'admin/items/index'
 end
