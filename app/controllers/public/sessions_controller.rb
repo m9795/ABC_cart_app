a0ce298d0ea8c81792e29b823a2de50d36f610b1
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-15 09:26:52 +0900

    [Add]カート画面編集,退会機能作成中

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -22,7 +22,17 @@ class Public::SessionsController < Devise::SessionsController
     public_items_path
   end
 
-  # protected
+  protected
+  def customer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if !@customer
+    
+    if @customer.valid_password?(params[:customer][:password])　&& (@customer.is_deleted == false)
+      redirect_to new_customer_registration
+    else
+      
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
