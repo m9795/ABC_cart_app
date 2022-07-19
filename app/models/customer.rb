51b0be975d6c78d6d081e07e75989d9c0b4e90a6
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-19 10:42:25 +0900

    [Add]enum導入

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,5 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :carts, dependent: :destroy
-  
-  enum is_deleted : { "": false, ""
+
 end
